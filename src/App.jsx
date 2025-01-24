3d79a80b2e3a8a3df741bdeefa56dcd94a5f6b6e
Julian <23019734@myrp.edu.sg>
2025-01-24 19:55:33 +0800

    update 24/1/2025

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Allprojects from './Pages/Allprojects';
 import Id1 from './Pages/IndvProj/id1';
 import Id2 from './Pages/IndvProj/id2'
 import Id3 from './Pages/IndvProj/id3'
-import Cursor from "../src/Components/cursor/Cursor";
+import Cursor from "../src/Components/cursor/Cursor"
 
 const App = () => {
     const [currentPage, setCurrentPage] = useState('home'); // 'home' or 'allprojects'
@@ -15,12 +15,14 @@ const App = () => {
 
     return (
         <div>
-            <Cursor />
+            <Cursor/>
             {currentPage === 'home' && <Home navigateTo={navigateTo} />}
             {currentPage === 'allprojects' && <Allprojects navigateTo={navigateTo} />}
             {currentPage === 'id1' && <Id1 navigateTo={navigateTo}/>}
             {currentPage === 'id2' && <Id2 navigateTo={navigateTo}/>}
             {currentPage === 'id3' && <Id3 navigateTo={navigateTo}/>}
+            {currentPage === 'id4' && <Id4 navigateTo={navigateTo}/>}
+            {currentPage === 'id5' && <Id5 navigateTo={navigateTo}/>}
         </div>
     );
 };
